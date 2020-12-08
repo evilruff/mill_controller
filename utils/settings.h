9151126b059a702954cada2e26be5e072deb13fb
brown <brown@amazeusgpl.com>
2020-12-08 11:42:34 +0300

    license added

--- utils/settings.h
+++ utils/settings.h
@@ -1,9 +1,23 @@
-/*
- * settings.h
+/**
+ *  Copyright 2020 by Yuri Alexandrov <evilruff@gmail.com>
  *
- * Created: 10/7/2020 4:50:56 PM
- *  Author: evilruff
- */ 
+ * This file is part of some open source application.
+ *
+ * Some open source application is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Some open source application is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QLogView.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ */
 
 
 #ifndef SETTINGS_H_
